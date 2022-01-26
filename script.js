8a8ac242d00974ca21a50ab64e89b32774eaf720
minhlong149 <minhlong9514@gmail.com>
2022-01-26 11:34:11 +0700

    Color Hover

--- script.js
+++ script.js
@@ -12,3 +12,35 @@ for (let indx = 0; indx < gridSize * gridSize; ++indx) {
   square.classList.add("square");
   container.appendChild(square);
 }
+
+// generate random RGB value color
+
+const colorHex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+function randomNumber() {
+  return Math.floor(Math.random() * colorHex.length);
+}
+
+function randomHexColor() {
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += colorHex[randomNumber()];
+  }
+  return color;
+}
+
+// grid divs change color when your mouse passes over
+
+const squares = document.querySelectorAll(".square");
+squares.forEach((square) => {
+  square.addEventListener("mouseover", () => {
+    square.style.backgroundColor = randomHexColor();
+  });
+});
+
+// A clear-current-grid button
+
+// Change the number of squares per side of the grid
+
+// [Opt] Having each pass another 10% of black
+// [Opt] Only after 10 passes is the square completely black
